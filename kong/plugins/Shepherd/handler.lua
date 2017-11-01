144377f4dfc9bba54032c7a9d6bbe5380bff49e2
Daniel T. Lee <danieltimlee@gmail.com>
2017-11-01 19:34:54 +0900

    Simple Structure For Plugin. Echo Header & Simple Migration Routine

--- kong/plugins/Shepherd/handler.lua
+++ kong/plugins/Shepherd/handler.lua
@@ -1,97 +1,74 @@
--- If you're not sure your plugin is executing, uncomment the line below and restart Kong
--- then it will throw an error which indicates the plugin is being loaded at least.
-
---assert(ngx.get_phase() == "timer", "The world is coming to an end!")
-
-
--- load the base plugin object and create a subclass
+local inspect = require('inspect')
 local plugin = require("kong.plugins.base_plugin"):extend()
 
--- constructor
-function plugin:new()
-  plugin.super.new(self, "Shepherd") --TODO: change "Shepherd" to the name of the plugin here
+delay = 1209600 -- Migration delay (14 days)
+req_header = nil;
 
-  -- do initialization here, runs in the 'init_by_lua_block', before worker processes are forked
+local function migrationWorker()
+  local ok, err = ngx.timer.at(delay, migrationHandler)
+  if not ok then
+    ngx.log(ngx.ERR, "failed to create the timer: ", err)
+    migrationWorker() -- TODO: Must Fix! Coded this, Due to Unknown Error
+    return
+  end
 end
 
----------------------------------------------------------------------------------------------
--- In the code below, just remove the opening brackets; `[[` to enable a specific handler
---
--- The handlers are based on the OpenResty handlers, see the OpenResty docs for details
--- on when exactly they are invoked and what limitations each handler has.
---
--- The call to `.super.xxx(self)` is a call to the base_plugin, which does nothing, except logging
--- that the specific handler was executed.
----------------------------------------------------------------------------------------------
+-- Premature for Timer off
+-- https://github.com/openresty/lua-nginx-module#ngxtimerat
+function migrationHandler(premature)
+  ngx.log(ngx.ERR, "Migration Work Start!")
+  -- TODO: Migration Work HERE.
+  if premature then
+    return
+  end
+  migrationWorker()
+end
 
 
---[[ handles more initialization, but AFTER the worker process has been forked/created.
--- It runs in the 'init_worker_by_lua_block'
+function plugin:new()
+  plugin.super.new(self, "Shepherd")
+end
+
+
 function plugin:init_worker()
   plugin.super.access(self)
-
-  -- your custom code here
-  
-end --]]
-
---[[ runs in the ssl_certificate_by_lua_block handler
-function plugin:certificate(plugin_conf)
-  plugin.super.access(self)
-
-  -- your custom code here
-  
-end --]]
-
---[[ runs in the 'rewrite_by_lua_block' (from version 0.10.2+)
--- IMPORTANT: during the `rewrite` phase neither the `api` nor the `consumer` will have
--- been identified, hence this handler will only be executed if the plugin is 
--- configured as a global plugin!
-function plugin:rewrite(plugin_conf)
-  plugin.super.rewrite(self)
-
-  -- your custom code here
-  
-end --]]
-
---- [[ runs in the 'access_by_lua_block'
-function plugin:access(plugin_conf)
-  plugin.super.access(self)
-
-  -- your custom code here
-  ngx.req.set_header("Hello-World", "this is on a request")
+  migrationWorker()
 end
 
---]]
 
---- [[ runs in the 'header_filter_by_lua_block'
+local function get_headers(header)
+  req_header = header
+end
+
+local function echo_headers()
+  for k, v in pairs(req_header) do
+    ngx.header[k] = v
+  end
+end
+
+
+function plugin:access(plugin_conf)
+  plugin.super.access(self)
+  ngx.log(ngx.ERR, inspect(plugin_conf))
+
+  get_headers(ngx.req.get_headers())
+  if req_header['version'] then
+    ngx.redirect("http://auth.dbrainscience.io")
+  end
+end
+
 function plugin:header_filter(plugin_conf)
   plugin.super.access(self)
 
-  -- your custom code here, for example;
-  ngx.header["Bye-World"] = "this is on the response"
+  echo_headers()
 end
 
---]]
-
---[[ runs in the 'body_filter_by_lua_block'
 function plugin:body_filter(plugin_conf)
   plugin.super.access(self)
-
   -- your custom code here
-  
-end --]]
+end
 
---[[ runs in the 'log_by_lua_block'
-function plugin:log(plugin_conf)
-  plugin.super.access(self)
+-- For Priority Upper than Req/Resp-Transformer
+plugin.PRIORITY = 850
 
-  -- your custom code here
-  
-end --]]
-
-
--- set the plugin priority, which determines plugin execution order
-plugin.PRIORITY = 1000
-
--- return our plugin object
 return plugin
--- kong/plugins/Shepherd/schema.lua
+++ kong/plugins/Shepherd/schema.lua
@@ -1,9 +1,17 @@
 return {
   no_consumer = false, -- this plugin is available on APIs as well as on Consumers,
   fields = {-- Describe your plugin's configuration's schema here.
+    migration_url = { type = "string", required = true }, -- S3 or Any Storage URL
+    migration_delay = { type = "number", immutable = true, required = true, default = 1209600 }, -- second
+    use_api_stage = { type = "boolean", default = true }, -- TEST, DEV, LIVE
+    use_client_ver = { type = "boolean", default = true },
+    global_stage = { type = "array", default = "LIVE", enum = { "TEST", "DEV", "LIVE" } }
   },
   self_check = function(schema, plugin_t, dao, is_updating)
     -- perform any custom verification
     return true
   end
 }
+
+
+-- TODO: Immutable not working
